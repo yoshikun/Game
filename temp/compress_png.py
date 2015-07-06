eddc3555eb79de5b0e2feb3c59b735657b210117
ttm.ok.db@163.com <ttm.ok.db@163.com>
2015-07-06 10:38:36 +0800

    update

--- temp/compress_png.py
+++ temp/compress_png.py
@@ -4,17 +4,19 @@ import numpy
 import Image
 import os, sys, shutil
 import zipfile
+import struct
 
 PNG = ".png"
 JPG = ".jpg"
-DATA = "_data"
-MASK = "_mask"
+DATA = ".data"
+MASK = ".mask"
+BIN = ".bin"
 MERGE = "_merge"
 COMPRESS = "_compress"
 
 #设置处理当前路径
-path = ".";
-#path = os.path.abspath(".")
+#path = "../Resources/ui/";
+path = os.path.abspath(".") + "/"
 total = len(os.listdir(path))
 
 print "Start decode total %d" % total
@@ -23,10 +25,12 @@ print "Start decode total %d" % total
 if os.path.isdir(path):
     #获取文件列表
     pngList = os.listdir(path)
+
     #遍历png
     for index in range(len(pngList)):
         #获取原图片
-        name = pngList[index]
+        name = path + pngList[index]
+        print name
         #解析文件名
         head = os.path.splitext(name)[0]
         #解析文件后缀
@@ -37,12 +41,9 @@ if os.path.isdir(path):
             ################################################################
             #拆分测试
             ################################################################
-            
+
             im = Image.open(name)
-            #获取原图片的通道
-            r, g, b, a = im.split()
-			
-			channel = len(im.split())
+            channel = len(im.split())
             #判断是否是有4个通道,因为策划会把jpg后缀改成png...
             if channel == 4:
 				#获取原图片的通道
@@ -57,15 +58,16 @@ if os.path.isdir(path):
             output = Image.fromarray(array)
             output.save(head + MASK, "JPEG")
 
-			"""
+            """
             #压缩
             output = zipfile.ZipFile(head + COMPRESS + PNG, "w", zipfile.ZIP_DEFLATED)
             output.write(head + DATA)
             output.write(head + MASK)
             output.close()
-            print "Decode \"%s\" complete" % name
-			"""
-
+            
+            """
+            
+            """
             ################################################################
             #合并测试
             ################################################################
@@ -82,8 +84,26 @@ if os.path.isdir(path):
             #输出
             im.save(head + MERGE + PNG, "PNG")
             print "Encode \"%s\" complete" % name
+            """
+
+            #合并
+            buffer = open(head + BIN, "wb")
+            
+            data = open(head + DATA, "rb").read()
+            size = os.path.getsize(head + DATA)
+            buffer.write(struct.pack('I', size))
+            buffer.write(data)
+            
+            mask = open(head + MASK, "rb").read()
+            size = os.path.getsize(head + MASK)
+            buffer.write(struct.pack('I', size))
+            buffer.write(mask)
+            
+            buffer.close()
 
             #os.remove(name)
             os.remove(head + DATA)
             os.remove(head + MASK)
-            os.remove(head + MERGE + PNG)
+            #os.remove(head + MERGE + PNG)
+
+            print "Decode \"%s\" complete" % name
